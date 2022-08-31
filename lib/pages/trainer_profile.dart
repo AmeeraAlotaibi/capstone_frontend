b5ef513a05e7ab29554bfbe97973847accfacb88
Ameera Alotaibi <meerareed.dev@outlook.com>
2022-08-31 23:29:38 +0300

    made sub button in trainer profile at the bottom of the screen

--- lib/pages/trainer_profile.dart
+++ lib/pages/trainer_profile.dart
@@ -14,57 +14,61 @@ class TrainerProfile extends StatelessWidget {
       ),
       body: SingleChildScrollView(
         child: Container(
-          width: double.infinity,
-          height: MediaQuery.of(context).size.height,
+          height: 600,
           margin: const EdgeInsets.only(top: 25),
           padding: const EdgeInsets.symmetric(horizontal: 25),
           child: Column(
             mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               // beginning of avatar card
-              TrainerProfileCard(
-                avatar:
-                    "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp",
-                fullName: "Dwight Schrutt",
-                username: "@DwightTHEGOAT",
-              ),
-              const SizedBox(
-                height: 25,
-              ),
+              Column(
+                children: [
+                  TrainerProfileCard(
+                    avatar:
+                        "https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp",
+                    fullName: "Dwight Schrutt",
+                    username: "@DwightTHEGOAT",
+                  ),
+                  const SizedBox(
+                    height: 25,
+                  ),
 
-              // details row
-              DetailsContainer(
-                title1: "Experience",
-                subtitle1: "3YRS",
-                title2: "Age",
-                subtitle2: "35YRS",
-                title3: "Specialty",
-                subtitle3: "Cross-fit",
-              ),
+                  // details row
+                  DetailsContainer(
+                    title1: "Experience",
+                    subtitle1: "3YRS",
+                    title2: "Age",
+                    subtitle2: "35YRS",
+                    title3: "Specialty",
+                    subtitle3: "Cross-fit",
+                  ),
 
-              const SizedBox(
-                height: 15,
-              ),
+                  const SizedBox(
+                    height: 15,
+                  ),
 
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 20),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      "About: ",
-                      style: Theme.of(context).textTheme.headline6,
+                  Container(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "About: ",
+                          style: Theme.of(context).textTheme.headline6,
+                        ),
+                        const SizedBox(
+                          height: 7.5,
+                        ),
+                        Text(
+                          "Lorem ipsum dolor ist, lorem lorenf kajfi faithe dfjlkei gljdfa, lorem ipsum is near us all, their fajfalkjei  lkjfiw.",
+                          style: Theme.of(context).textTheme.bodyText1,
+                        ),
+                      ],
                     ),
-                    const SizedBox(
-                      height: 7.5,
-                    ),
-                    Text(
-                      "Lorem ipsum dolor ist, lorem lorenf kajfi faithe dfjlkei gljdfa, lorem ipsum is near us all, their fajfalkjei  lkjfiw.",
-                      style: Theme.of(context).textTheme.bodyText1,
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
               const SizedBox(
                 height: 25,
@@ -75,7 +79,7 @@ class TrainerProfile extends StatelessWidget {
                 onPressed: () {
                   // subscribe button function here
                 },
-              )
+              ),
             ],
           ),
         ),
