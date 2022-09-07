f3f24d5a56bc47518caf631f5ae90a893f61eea3
Abdullah <adbdb.27@gmail.com>
2022-09-07 21:46:00 +0300

    search

--- lib/widgets/generic/search_bar.dart
+++ lib/widgets/generic/search_bar.dart
@@ -1,4 +1,11 @@
+
+import 'package:capstone_frontend/models/trainer.dart';
+import 'package:capstone_frontend/pages/trainer_profile.dart';
+import 'package:capstone_frontend/providers/trainer_provider.dart';
+import 'package:capstone_frontend/services/trainer_service.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_typeahead/flutter_typeahead.dart';
+import 'package:provider/provider.dart';
 
 class SearchBar extends StatelessWidget {
   SearchBar({
@@ -11,25 +18,53 @@ class SearchBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      decoration: BoxDecoration(
-        boxShadow: [
-          BoxShadow(
-            offset: const Offset(0, 5),
-            spreadRadius: -13,
-            blurRadius: 28,
-            color: Theme.of(context).shadowColor,
+            padding: const EdgeInsets.all(16),
+            child: TypeAheadField<Trainer>(
+  textFieldConfiguration: const TextFieldConfiguration(
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.search),
+                    border: OutlineInputBorder(),
+                    hintText: 'Search Trainer',
+                  ),
+                ),
+  suggestionsCallback:(pattern) async {
+      return await context.read<TrainerProvider>().search(pattern);
+  }, 
+   noItemsFoundBuilder: (context) => const SizedBox(
+                  height: 100,
+                  child:  Center(
+                    child: Text(
+                      'No Users Found.',
+                      style: TextStyle(fontSize: 24),
+                    ),
+                  ),
+                ),
+  itemBuilder: (context, suggestion) {
+      return Container(
+        color: Theme.of(context).cardColor ,
+        child: ListTile(
+          leading: SizedBox(
+            width: 50,
+            height: 50,
+             child: Image.network(suggestion.image ??
+                                      "https://millingtontownship.com/wp-content/uploads/2021/01/default.jpg"
+                                , fit: BoxFit.cover,),
           ),
-        ],
-      ),
-      child: TextField(
-        controller: controller,
-        decoration: const InputDecoration(
-          hintText: "Search Trainers",
-          suffixIcon: Icon(
-            Icons.search,
-          ),
+          title: Text(suggestion.user.first_name!, style: Theme.of(context).textTheme.bodyText2 ,)
+          // subtitle: Text(suggestion),
+          
         ),
+      );
+  },
+  onSuggestionSelected: (suggestion) {
+      Navigator.of(context).push(MaterialPageRoute(
+        builder: (context) => TrainerProfile(trainerId: suggestion.user.id)
+     
       ),
+      ); 
+     
+  },
+),
     );
   }
 }
